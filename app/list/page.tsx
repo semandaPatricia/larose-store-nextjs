8ab0059505fcde305d054ea86cb13deac5d4ce70
semanda <patash248@gmail.com>
2024-05-23 09:09:34 +0200

    add single product page

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -18,7 +18,7 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
       <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
         <div className="w-2/3 flex flex-col items-center justify-center gap-8">
           <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
-            Grab up to 50% off on
+            Grab up to 20% off on
             <br /> Selected Products
           </h1>
           <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
